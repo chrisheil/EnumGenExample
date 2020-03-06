1a21c3cc5b8fba30feec6293093bdee5232f06cb
Chris Heil <chris.heil@urbanft.com>
2020-03-06 11:31:22 -0600

    Initial commit

---
+++ EnumGenExample/Controllers/DemoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EnumGenExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery]DemoRequest request)
+        {
+            return Ok();
+        }
+    }
+
+    public class DemoRequest
+    {
+        public DemoEnum DemoSelection { get; set; }
+        public DemoEnum? DemoSelectionNullable { get; set; }
+    }
+
+    public enum DemoEnum
+    {
+        value1,
+        value2
+    }
+}
